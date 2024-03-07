13b7e52b15d477f667554f0c8a553ea17d4ed48a
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-08 01:23:59 +0900

    あたらしいAPIを利用するように変更

--- tjpw_schedule/interface/gas/gas_api.py
+++ tjpw_schedule/interface/gas/gas_api.py
@@ -2,15 +2,19 @@ import os
 import requests
 from tjpw_schedule.domain.schedule import TournamentSchedule
 from datetime import timedelta
+from tjpw_schedule.custom_logging import get_logger
+import json
+
+logger = get_logger(__name__)
 
 
 class GasApi:
     def __init__(self) -> None:
-        self.domain = os.environ["DAILY_API_DOMAIN"] + "/gas"
+        self.domain = os.environ["LAMBDA_GOOGLE_CALENDAR_API_DOMAIN"]
 
     def regist_tournament_schedule(self, schedule: TournamentSchedule):
-        """ スケジュールを登録 """
-        print(schedule)
+        """スケジュールを登録"""
+        logger.debug(schedule)
         start = schedule.open_datetime
         # 終了時刻は開場時刻からとりあえず4時間後
         end = start + timedelta(hours=4)
@@ -21,21 +25,43 @@ class GasApi:
             "end": end.isoformat(),
             "detail": schedule.convert_to_detail(),
         }
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        }
 
-        response = requests.post(url=self.domain + "/calendar/",
-                                 json=json_data)
-        return response.json()
+        response = requests.post(
+            url=self.domain + "schedule", headers=headers, json=json_data
+        )
+        response_json = response.json()
+        if isinstance(response_json, str):
+            response_json = json.loads(response_json)
+        logger.debug(f"post_gas_calendar: response={response_json}")
+        return response_json
 
 
 if __name__ == "__main__":
     # python -m tjpw_schedule.interface.gas.gas_api
-    from tjpw_schedule.domain.schedule import TournamentSchedule, TournamentName, Date, Venue, SeatType, Note
+    from tjpw_schedule.domain.schedule import (
+        TournamentSchedule,
+        TournamentName,
+        Date,
+        Venue,
+        SeatType,
+        Note,
+    )
+
     suite = GasApi()
     schedule = TournamentSchedule(
-        url='https://www.ddtpro.com/schedules/20450',
+        url="https://www.ddtpro.com/schedules/20450",
         tournament_name=TournamentName(value="TJPW CITY CIRCUIT '23～仙台再上陸～"),
-        date=Date(value='2023年10月21日(土)\u3000開場12:30\u3000開始13:00'),
-        venue=Venue(value='宮城・夢メッセみやぎ西館ホール（仙台市）'),
-        seat_type=SeatType(value='■チケット\n－前売り－\nスーパーシート8,500円\u3000完売\n特別リングサイド6,500円\n指定席4,500円\nレディースシート2,000円\n－当日－\n特別リングサイド7,000円\n指定席5,000円\nレディースシート2,500円\nU-18チケット1,000円（要身分証）\n\n■販売場所\n公式チケット購入フォーム、チケットぴあ、ローソンチケット、e+\n※UNIVERSE会員先行受付＝7月20日(木)12:00～7月24日(月)12:00、一般販売＝8月6日(日)～。'),
-        note=Note(value='■全カード\n○メインイベント\n瑞希＆遠藤有栖\u3000vs\u3000伊藤麻希＆上福ゆき\n○マックス・ジ・インペイラー＆愛野ユキ＆原宿ぽむ\u3000vs\u3000辰巳リカ＆渡辺未詩＆上原わかな\n○乃蒼ヒカリ＆角田奈穂\u3000vs\u3000宮本もか＆鳥喰かや\n○レジーナ\u3000vs\u3000HIMAWARI\n○中島翔子＆ハイパーミサヲ\u3000vs\u3000凍雅＆風城ハル\n○3WAYマッチ\nらく\u3000vs\u3000桐生真弥\u3000vs\u3000鈴木志乃\n○鈴芽\u3000vs\u3000大久保琉那'))
+        date=Date(value="2023年10月21日(土)\u3000開場12:30\u3000開始13:00"),
+        venue=Venue(value="宮城・夢メッセみやぎ西館ホール（仙台市）"),
+        seat_type=SeatType(
+            value="■チケット\n－前売り－\nスーパーシート8,500円\u3000完売\n特別リングサイド6,500円\n指定席4,500円\nレディースシート2,000円\n－当日－\n特別リングサイド7,000円\n指定席5,000円\nレディースシート2,500円\nU-18チケット1,000円（要身分証）\n\n■販売場所\n公式チケット購入フォーム、チケットぴあ、ローソンチケット、e+\n※UNIVERSE会員先行受付＝7月20日(木)12:00～7月24日(月)12:00、一般販売＝8月6日(日)～。"
+        ),
+        note=Note(
+            value="■全カード\n○メインイベント\n瑞希＆遠藤有栖\u3000vs\u3000伊藤麻希＆上福ゆき\n○マックス・ジ・インペイラー＆愛野ユキ＆原宿ぽむ\u3000vs\u3000辰巳リカ＆渡辺未詩＆上原わかな\n○乃蒼ヒカリ＆角田奈穂\u3000vs\u3000宮本もか＆鳥喰かや\n○レジーナ\u3000vs\u3000HIMAWARI\n○中島翔子＆ハイパーミサヲ\u3000vs\u3000凍雅＆風城ハル\n○3WAYマッチ\nらく\u3000vs\u3000桐生真弥\u3000vs\u3000鈴木志乃\n○鈴芽\u3000vs\u3000大久保琉那"
+        ),
+    )
     suite.regist_tournament_schedule(schedule)
--- tjpw_schedule/interface/notion/notion_api.py
+++ tjpw_schedule/interface/notion/notion_api.py
@@ -1,20 +1,40 @@
 import os
 import requests
 from tjpw_schedule.domain.schedule import TournamentSchedule
+from tjpw_schedule.custom_logging import get_logger
+import json
+
+logger = get_logger(__name__)
 
 
 class NotionApi:
     def __init__(self) -> None:
-        self.domain = os.environ["DAILY_API_DOMAIN"] + "/notion"
+        self.domain = os.environ["LAMBDA_NOTION_API_DOMAIN"]
+        self.notion_secret = os.environ["NOTION_SECRET"]
 
     def regist_tournament_schedule(self, schedule: TournamentSchedule):
-        """ スケジュールを登録 """
-        print(schedule)
-        json_data = {
+        """スケジュールを登録"""
+        logger.debug(schedule)
+        data = {
+            "url": schedule.url,
             "title": schedule.tournament_name.value,
             "date": schedule.date.isoformatted_date,
-            "url": schedule.url,
+            "promotion": "東京女子プロレス",
+            "tags": [],
         }
-        response = requests.post(url=self.domain + "/prowrestling/",
-                                 json=json_data)
-        return response.json()
+
+        return self._post(url=self.domain + "prowrestling", data=data)
+
+    def _post(self, url: str, data: dict) -> dict:
+        headers = {
+            "access-token": self.notion_secret,
+        }
+        logger.debug(f"url: {url} data: {json.dumps(data, ensure_ascii=False)}")
+        respone = requests.post(url=url, headers=headers, json=data)
+        if respone.status_code != 200:
+            raise Exception(
+                f"status_code: {respone.status_code}, message: {respone.text}"
+            )
+        response_json = respone.json()
+        logger.debug(json.dumps(response_json, ensure_ascii=False))
+        return response_json["data"]
