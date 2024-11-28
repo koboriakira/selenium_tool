c97e1f8adc6970f2982997b978ecf96f0016c686
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-11-28 12:43:25 +0900

    refactor: Remove unnecessary files and configurations

--- cdk/bin/cdk.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { TjpwScheduleStack } from '../lib/tjpw-schedule-stack';
-
-const app = new cdk.App();
-new TjpwScheduleStack(app, 'TjpwScheduleStack', {});
