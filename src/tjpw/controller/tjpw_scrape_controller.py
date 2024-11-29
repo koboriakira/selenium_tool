20ad07a611d929700b41d8b35e0418e40a1fb1f3
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-11-29 16:45:43 +0900

    refactor: simplify SeleniumScraper by removing direct Selenium driver initialization and using dependency injection

--- src/tjpw/controller/tjpw_scrape_controller.py
+++ src/tjpw/controller/tjpw_scrape_controller.py
@@ -6,6 +6,8 @@ from src.tjpw.infrastructure.selenium_scraper import SeleniumScraper
 from src.tjpw.usecase.request.scrape_range import ScrapeRange
 from src.tjpw.usecase.scrape_tjpw import ScrapeTjpw
 
+from common.selenium_factory import SeleniumFactory
+
 JST = timezone(timedelta(hours=+9), "JST")
 logger = get_logger(__name__)
 
@@ -14,12 +16,18 @@ class TjpwScrapeController:
     def scrape(self) -> None:
         logger.debug("Start main function")
         range = ScrapeRange.create_default_instance()
-        scrape_tjpw_usecase = ScrapeTjpw(
-            scraper=SeleniumScraper(),
-            schedule_external_api_list=[ScheduleGoogleCalendarApi()],
-        )
-        scrape_tjpw_usecase.execute(range)
-        logger.debug("End main function")
+        try:
+            driver = SeleniumFactory().get_driver()
+            scraper = SeleniumScraper(driver=driver)
+            scrape_tjpw_usecase = ScrapeTjpw(
+                scraper=scraper,
+                schedule_external_api_list=[ScheduleGoogleCalendarApi()],
+            )
+            scrape_tjpw_usecase.execute(range)
+            logger.debug("End main function")
+            driver.quit()
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":
--- src/tjpw/infrastructure/selenium_scraper.py
+++ src/tjpw/infrastructure/selenium_scraper.py
@@ -1,11 +1,9 @@
 import os
 from datetime import datetime
 
-import requests
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
-
 from src.custom_logging import get_logger
 from src.tjpw.domain.scraper import ActiveTableItems, DetailUrl, Scraper
 from src.tjpw.infrastructure.Item_entity import ItemEntity
@@ -16,15 +14,9 @@ WAIT_TIME = 5
 
 
 class SeleniumScraper(Scraper):
-    def __init__(self, selenium_domain: str | None = None):
+    def __init__(self, driver: WebDriver) -> None:
         # Seleniumが起動しているか確認
-        self._selenium_domain = selenium_domain or SELENIUM_DOMAIN
-        selenium_url = self._selenium_domain + "/wd/hub/status"
-        response = requests.get(selenium_url)
-        if not response.ok or not response.json()["value"]["ready"]:
-            msg = "Selenium is not ready. url: " + selenium_url
-            logger.exception(msg)
-            raise Exception(msg)
+        self._driver = driver
 
     def get_detail_urls(
         self,
@@ -32,56 +24,40 @@ class SeleniumScraper(Scraper):
         target_month: int,
     ) -> list[DetailUrl]:
         url = self._generate_get_detail_api_url(target_year, target_month)
-        logger.debug(url)
-        driver = _get_driver(self._selenium_domain)
-        try:
-            driver.get(url)
-            elements = driver.find_elements(By.CLASS_NAME, "Itemrow__content")
-            return [_extract_detail_url_info(element) for element in elements]
-        finally:
-            driver.quit()
+        print(url)
+        self._driver.get(url)
+        elements = self._driver.find_elements(By.CLASS_NAME, "Itemrow__content")
+        details = [_extract_detail_url_info(element) for element in elements]
+        return [detail for detail in details if detail is not None]
 
     def scrape_detail(self, url: str) -> ItemEntity:
         """試合詳細を取得"""
-        logger.debug(url)
-        driver = _get_driver(self._selenium_domain)
-        try:
-            driver.get(url)
-            elements = driver.find_elements(By.CLASS_NAME, "Article_Table__item")
-            active_table_items = ActiveTableItems.from_web_elements(url, elements)
-            return active_table_items.to_entity_with_url()
-        finally:
-            driver.quit()
+        print(url)
+        self._driver.get(url)
+        elements = self._driver.find_elements(By.CLASS_NAME, "Article_Table__item")
+        active_table_items = ActiveTableItems.from_web_elements(url, elements)
+        return active_table_items.to_entity_with_url()
 
     def _generate_get_detail_api_url(self, target_year: int, target_month: int) -> str:
         """URLを生成"""
         params = {"teamId": "tjpw", "yyyymm": f"{target_year}{target_month:02}"}
-        return (
-            self.DDTPRO_SCHEDULES
-            + "?"
-            + "&".join([f"{key}={value}" for key, value in params.items()])
-        )
+        return self.DDTPRO_SCHEDULES + "?" + "&".join([f"{key}={value}" for key, value in params.items()])
 
 
 def _convert_to_date(date_str: str) -> datetime:
     """
     "2024Year3Month2Date(Saturday)"のような日付文字列をdatetimeに変換
     """
-    return datetime.strptime(date_str, "%YYear%mMonth%dDate(%A)")
+    return datetime.strptime(date_str, "%YYear%mMonth%dDate(%A)")  # noqa: DTZ007
 
 
-def _extract_detail_url_info(element: WebElement) -> DetailUrl:
-    href = element.get_attribute("href")
+def _extract_detail_url_info(element: WebElement) -> DetailUrl | None:
+    href = element.get_attribute("href") or ""
+    if "schedules" not in href:
+        title_text = element.find_element(By.CLASS_NAME, "Itemrow__title").text
+        print(f"試合以外の予定のため除外 {title_text}")
+        return None
+
     date_row = element.find_element(By.CLASS_NAME, "Itemrow__date")
     event_datetime = _convert_to_date(date_row.text)
     return DetailUrl(value=href, date=event_datetime)
-
-
-def _get_driver(selenium_domain: str):
-    """Seleniumのドライバーを取得"""
-    driver = webdriver.Remote(
-        command_executor=selenium_domain,
-        options=webdriver.ChromeOptions(),
-    )
-    driver.implicitly_wait(WAIT_TIME)
-    return driver
